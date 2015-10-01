ad2e441a080f6fec87075934995fa7aceb6870d5
Brad Berger <brad@bradb.net>
2015-10-01 15:47:01 +0200

    Gofmt and adds ViewportWidth and manual Optimize call

--- optimizer.go
+++ optimizer.go
@@ -1,9 +1,9 @@
 package optimizer
 
 import (
+	"fmt"
 	"github.com/bradberger/resize"
 	"github.com/bradberger/webp"
-	"fmt"
 	"image"
 	"image/gif"
 	"image/jpeg"
@@ -19,35 +19,52 @@ type Options struct {
 	Dpr           float64
 	Quality       int
 	Downlink      float64
+	ViewportWidth float64
 	SaveData      bool
 	Interpolation resize.InterpolationFunction
+	Optimized     bool
+}
+
+func (o *Options) Optimize() {
+
+	if !o.Optimized {
+
+		if o.Width > 0 {
+			o.Width = uint(float64(o.Width) * o.Dpr)
+		}
+
+		if o.Dpr == 0 {
+			o.Dpr = 1
+		}
+
+		// If quality not explicity set, we'll try to optimize it.
+		if o.Quality == 0 {
+
+			o.Quality = int(100 - o.Dpr*30)
+
+			if o.Downlink > 0 && o.Downlink < 1 {
+				o.Quality = int(float32(o.Quality) * float32(o.Downlink))
+			}
+
+			if o.SaveData {
+				o.Quality = int(float32(o.Quality) * float32(0.75))
+			}
+
+		}
+
+		o.Optimized = true
+
+	}
+
 }
 
 // Encode encodes the image with the given options.
 func Encode(w io.Writer, i image.Image, o Options) error {
 
+	o.Optimize()
+
 	if o.Width > 0 {
 		i = resize.Resize(uint(float64(o.Width)*o.Dpr), o.Height, i, resize.Bicubic)
-	}
-
-	// DPR should default to 1
-	if o.Dpr == 0 {
-		o.Dpr = 1
-	}
-
-	// If quality not explicity set, we'll try to optimize it.
-	if o.Quality == 0 {
-
-		o.Quality = int(100 - o.Dpr*30)
-
-		if o.Downlink > 0 && o.Downlink < 1 {
-			o.Quality = int(float32(o.Quality) * float32(o.Downlink))
-		}
-
-		if o.SaveData {
-			o.Quality = int(float32(o.Quality) * float32(0.75))
-		}
-
 	}
 
 	// Now write the result.
